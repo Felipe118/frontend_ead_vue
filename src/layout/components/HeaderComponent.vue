8b428a62a51285b2c948b20fc9379b63f7c470b8
Felipe118 <lipebezerraa@gmail.com>
2022-11-29 22:44:11 -0300

    ajustes

--- src/layout/components/HeaderComponent.vue
+++ src/layout/components/HeaderComponent.vue
@@ -2,9 +2,12 @@
    <header id="header">
         <nav>
             <span class="toggleMenu far fa-bars"></span>
-            <span class="logo">
-                <img :src="require('@/assets/images/logo.svg')" alt="EspecializaTi">
-            </span>
+            <router-link :to="{name:campus.home}" >
+                <span class="logo">
+                    <img :src="require('@/assets/images/logo.svg')" alt="EspecializaTi">
+                </span>
+            </router-link>
+           
         </nav>
     </header>
 </template>
