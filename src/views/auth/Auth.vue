ec949f227746390389f37ac18d08ea36cc8c0361
Felipe118 <lipebezerraa@gmail.com>
2022-12-15 23:19:37 -0300

    feat: criando o login

--- src/views/auth/Auth.vue
+++ src/views/auth/Auth.vue
@@ -64,7 +64,7 @@ export default {
         const store = useStore()
         //const login = () => router.push({name: 'campus.home'})
 
-        const auth () => {
+        const auth = () => {
             store.dispatch('auth', {
                 email: 'lipebezerraa@gmail.com',
                 password: '123'
